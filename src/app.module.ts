6de0eca505b33bfd4d212a8ecfe6f959a52834b2
Tan Tiong Guan <tantiongguan123@gmail.com>
2022-10-20 20:24:38 +0800

    feat(setup): Setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseConfigurationService } from './configuration/typeorm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigurationService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
